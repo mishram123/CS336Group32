b16f4631d41ef9333bd10cee10ed99760ac3dd3f
Mannan Mishra <mm3033@scarletmail.rutgers.edu>
2023-12-04 15:01:27 -0500

    I added a bunch of tables and populated many as well for aircraft, tickets, etc...

--- FlightReservationSystem.sql
+++ FlightReservationSystem.sql
@@ -42,6 +42,58 @@ LOCK TABLES `account` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `aircraft`
+--
+
+DROP TABLE IF EXISTS `aircraft`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `aircraft` (
+  `AircraftID` varchar(10) NOT NULL,
+  `seats` int DEFAULT NULL,
+  `operation_days` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`AircraftID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `aircraft`
+--
+
+LOCK TABLES `aircraft` WRITE;
+/*!40000 ALTER TABLE `aircraft` DISABLE KEYS */;
+INSERT INTO `aircraft` VALUES ('A001',150,'Monday,Wednesday,Friday'),('A002',200,'Tuesday,Thursday,Saturday,Sunday'),('A003',180,'Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday'),('A004',160,'Monday,Thursday,Saturday'),('A005',190,'Tuesday,Friday,Sunday'),('A006',170,'Monday,Tuesday,Thursday,Saturday,Sunday'),('A007',220,'Wednesday,Saturday,Sunday'),('A008',140,'Tuesday,Wednesday,Friday,Saturday'),('A009',210,'Monday,Wednesday,Saturday,Sunday'),('A010',180,'Tuesday,Thursday,Sunday'),('A011',160,'Monday,Wednesday,Friday,Saturday'),('A012',190,'Tuesday,Thursday,Sunday'),('A013',180,'Monday,Wednesday,Saturday,Sunday'),('A014',170,'Tuesday,Thursday,Friday,Sunday'),('A015',200,'Monday,Wednesday,Friday,Sunday');
+/*!40000 ALTER TABLE `aircraft` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `airlineairportassociatedwith`
+--
+
+DROP TABLE IF EXISTS `airlineairportassociatedwith`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `airlineairportassociatedwith` (
+  `ThreeLetterID` varchar(3) NOT NULL,
+  `TwoLetterID` varchar(2) NOT NULL,
+  PRIMARY KEY (`ThreeLetterID`,`TwoLetterID`),
+  KEY `TwoLetterID` (`TwoLetterID`),
+  CONSTRAINT `airlineairportassociatedwith_ibfk_1` FOREIGN KEY (`ThreeLetterID`) REFERENCES `airport` (`ThreeLetterID`),
+  CONSTRAINT `airlineairportassociatedwith_ibfk_2` FOREIGN KEY (`TwoLetterID`) REFERENCES `airlinecompany` (`TwoLetterID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `airlineairportassociatedwith`
+--
+
+LOCK TABLES `airlineairportassociatedwith` WRITE;
+/*!40000 ALTER TABLE `airlineairportassociatedwith` DISABLE KEYS */;
+INSERT INTO `airlineairportassociatedwith` VALUES ('DFW','AA'),('JFK','AA'),('LHR','BA'),('ATL','DL'),('LAX','DL'),('DXB','EK'),('EWR','UA'),('ORD','UA'),('PEK','UA'),('SFO','UA');
+/*!40000 ALTER TABLE `airlineairportassociatedwith` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `airlinecompany`
 --
 
@@ -138,6 +190,220 @@ INSERT INTO `customerrepresentative` VALUES ('Rep1','Rep1');
 UNLOCK TABLES;
 
 --
+-- Table structure for table `departurearrival`
+--
+
+DROP TABLE IF EXISTS `departurearrival`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `departurearrival` (
+  `flightNumber` varchar(5) NOT NULL,
+  `DepartureThreeLetterID` varchar(3) NOT NULL,
+  `ArrivalThreeLetterID` varchar(3) NOT NULL,
+  PRIMARY KEY (`flightNumber`,`DepartureThreeLetterID`,`ArrivalThreeLetterID`),
+  KEY `DepartureThreeLetterID` (`DepartureThreeLetterID`),
+  KEY `ArrivalThreeLetterID` (`ArrivalThreeLetterID`),
+  CONSTRAINT `departurearrival_ibfk_1` FOREIGN KEY (`flightNumber`) REFERENCES `flightservices` (`flightNumber`),
+  CONSTRAINT `departurearrival_ibfk_2` FOREIGN KEY (`DepartureThreeLetterID`) REFERENCES `airport` (`ThreeLetterID`),
+  CONSTRAINT `departurearrival_ibfk_3` FOREIGN KEY (`ArrivalThreeLetterID`) REFERENCES `airport` (`ThreeLetterID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `departurearrival`
+--
+
+LOCK TABLES `departurearrival` WRITE;
+/*!40000 ALTER TABLE `departurearrival` DISABLE KEYS */;
+/*!40000 ALTER TABLE `departurearrival` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `domesticflight`
+--
+
+DROP TABLE IF EXISTS `domesticflight`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `domesticflight` (
+  `flightNumber` varchar(5) NOT NULL,
+  `AircraftID` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`flightNumber`),
+  KEY `AircraftID` (`AircraftID`),
+  CONSTRAINT `domesticflight_ibfk_1` FOREIGN KEY (`AircraftID`) REFERENCES `aircraft` (`AircraftID`),
+  CONSTRAINT `domesticflight_ibfk_2` FOREIGN KEY (`flightNumber`) REFERENCES `flightservices` (`flightNumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `domesticflight`
+--
+
+LOCK TABLES `domesticflight` WRITE;
+/*!40000 ALTER TABLE `domesticflight` DISABLE KEYS */;
+/*!40000 ALTER TABLE `domesticflight` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `flightservices`
+--
+
+DROP TABLE IF EXISTS `flightservices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `flightservices` (
+  `flightNumber` varchar(5) NOT NULL,
+  `AircraftID` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`flightNumber`),
+  KEY `AircraftID` (`AircraftID`),
+  CONSTRAINT `flightservices_ibfk_1` FOREIGN KEY (`AircraftID`) REFERENCES `aircraft` (`AircraftID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `flightservices`
+--
+
+LOCK TABLES `flightservices` WRITE;
+/*!40000 ALTER TABLE `flightservices` DISABLE KEYS */;
+/*!40000 ALTER TABLE `flightservices` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `internationalflight`
+--
+
+DROP TABLE IF EXISTS `internationalflight`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `internationalflight` (
+  `flightNumber` varchar(5) NOT NULL,
+  `AircraftID` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`flightNumber`),
+  KEY `AircraftID` (`AircraftID`),
+  CONSTRAINT `internationalflight_ibfk_1` FOREIGN KEY (`AircraftID`) REFERENCES `aircraft` (`AircraftID`),
+  CONSTRAINT `internationalflight_ibfk_2` FOREIGN KEY (`flightNumber`) REFERENCES `flightservices` (`flightNumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `internationalflight`
+--
+
+LOCK TABLES `internationalflight` WRITE;
+/*!40000 ALTER TABLE `internationalflight` DISABLE KEYS */;
+/*!40000 ALTER TABLE `internationalflight` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `onewayticket`
+--
+
+DROP TABLE IF EXISTS `onewayticket`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `onewayticket` (
+  `TicketNumber` varchar(10) NOT NULL,
+  `DepartureFlightNumber` varchar(5) NOT NULL,
+  PRIMARY KEY (`TicketNumber`),
+  KEY `DepartureFlightNumber` (`DepartureFlightNumber`),
+  CONSTRAINT `onewayticket_ibfk_1` FOREIGN KEY (`TicketNumber`) REFERENCES `ticket` (`TicketNumber`),
+  CONSTRAINT `onewayticket_ibfk_2` FOREIGN KEY (`DepartureFlightNumber`) REFERENCES `flightservices` (`flightNumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `onewayticket`
+--
+
+LOCK TABLES `onewayticket` WRITE;
+/*!40000 ALTER TABLE `onewayticket` DISABLE KEYS */;
+/*!40000 ALTER TABLE `onewayticket` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `operates`
+--
+
+DROP TABLE IF EXISTS `operates`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `operates` (
+  `TwoLetterID` varchar(2) NOT NULL,
+  `flightNumber` varchar(5) NOT NULL,
+  PRIMARY KEY (`TwoLetterID`,`flightNumber`),
+  KEY `flightNumber` (`flightNumber`),
+  CONSTRAINT `operates_ibfk_1` FOREIGN KEY (`TwoLetterID`) REFERENCES `airlinecompany` (`TwoLetterID`),
+  CONSTRAINT `operates_ibfk_2` FOREIGN KEY (`flightNumber`) REFERENCES `flightservices` (`flightNumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `operates`
+--
+
+LOCK TABLES `operates` WRITE;
+/*!40000 ALTER TABLE `operates` DISABLE KEYS */;
+/*!40000 ALTER TABLE `operates` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `owns`
+--
+
+DROP TABLE IF EXISTS `owns`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `owns` (
+  `TwoLetterID` varchar(2) NOT NULL,
+  `AircraftID` varchar(10) NOT NULL,
+  PRIMARY KEY (`TwoLetterID`,`AircraftID`),
+  KEY `AircraftID` (`AircraftID`),
+  CONSTRAINT `owns_ibfk_1` FOREIGN KEY (`TwoLetterID`) REFERENCES `airlinecompany` (`TwoLetterID`),
+  CONSTRAINT `owns_ibfk_2` FOREIGN KEY (`AircraftID`) REFERENCES `aircraft` (`AircraftID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `owns`
+--
+
+LOCK TABLES `owns` WRITE;
+/*!40000 ALTER TABLE `owns` DISABLE KEYS */;
+INSERT INTO `owns` VALUES ('AA','A001'),('AF','A002'),('BA','A003'),('DL','A004'),('EK','A005'),('LH','A006'),('QF','A007'),('SQ','A008'),('UA','A009'),('AA','A010'),('AF','A011'),('BA','A012'),('DL','A013'),('EK','A014'),('LH','A015');
+/*!40000 ALTER TABLE `owns` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roundtripticket`
+--
+
+DROP TABLE IF EXISTS `roundtripticket`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roundtripticket` (
+  `TicketNumber` varchar(10) NOT NULL,
+  `DepartureFlightNumber` varchar(5) NOT NULL,
+  `ReturnFlightNumber` varchar(5) NOT NULL,
+  PRIMARY KEY (`TicketNumber`),
+  KEY `DepartureFlightNumber` (`DepartureFlightNumber`),
+  KEY `ReturnFlightNumber` (`ReturnFlightNumber`),
+  CONSTRAINT `roundtripticket_ibfk_1` FOREIGN KEY (`TicketNumber`) REFERENCES `ticket` (`TicketNumber`),
+  CONSTRAINT `roundtripticket_ibfk_2` FOREIGN KEY (`DepartureFlightNumber`) REFERENCES `flightservices` (`flightNumber`),
+  CONSTRAINT `roundtripticket_ibfk_3` FOREIGN KEY (`ReturnFlightNumber`) REFERENCES `flightservices` (`flightNumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roundtripticket`
+--
+
+LOCK TABLES `roundtripticket` WRITE;
+/*!40000 ALTER TABLE `roundtripticket` DISABLE KEYS */;
+/*!40000 ALTER TABLE `roundtripticket` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `siteadmin`
 --
 
@@ -160,6 +426,72 @@ LOCK TABLES `siteadmin` WRITE;
 /*!40000 ALTER TABLE `siteadmin` DISABLE KEYS */;
 INSERT INTO `siteadmin` VALUES ('Admin1','Admin1');
 /*!40000 ALTER TABLE `siteadmin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ticket`
+--
+
+DROP TABLE IF EXISTS `ticket`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ticket` (
+  `TicketNumber` varchar(10) NOT NULL,
+  `seatNumber` varchar(3) DEFAULT NULL,
+  `total_fare` float DEFAULT NULL,
+  `bookingFee` float DEFAULT NULL,
+  `purchaseDateTime` datetime DEFAULT NULL,
+  `Passenger_Name` varchar(50) DEFAULT NULL,
+  `first_name` varchar(25) DEFAULT NULL,
+  `last_name` varchar(25) DEFAULT NULL,
+  `class` varchar(10) DEFAULT NULL,
+  `isEconomy` tinyint(1) DEFAULT NULL,
+  `changeCancelFee` float DEFAULT NULL,
+  PRIMARY KEY (`TicketNumber`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ticket`
+--
+
+LOCK TABLES `ticket` WRITE;
+/*!40000 ALTER TABLE `ticket` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ticket` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ticketflightassociatedwith`
+--
+
+DROP TABLE IF EXISTS `ticketflightassociatedwith`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ticketflightassociatedwith` (
+  `TicketNumber` varchar(10) NOT NULL,
+  `flightNumber` varchar(5) NOT NULL,
+  `fromAirport` varchar(3) DEFAULT NULL,
+  `toAirport` varchar(3) DEFAULT NULL,
+  `departureDate` date DEFAULT NULL,
+  `departureTime` time DEFAULT NULL,
+  PRIMARY KEY (`TicketNumber`,`flightNumber`),
+  KEY `flightNumber` (`flightNumber`),
+  KEY `fromAirport` (`fromAirport`),
+  KEY `toAirport` (`toAirport`),
+  CONSTRAINT `ticketflightassociatedwith_ibfk_1` FOREIGN KEY (`TicketNumber`) REFERENCES `ticket` (`TicketNumber`),
+  CONSTRAINT `ticketflightassociatedwith_ibfk_2` FOREIGN KEY (`flightNumber`) REFERENCES `flightservices` (`flightNumber`),
+  CONSTRAINT `ticketflightassociatedwith_ibfk_3` FOREIGN KEY (`fromAirport`) REFERENCES `airport` (`ThreeLetterID`),
+  CONSTRAINT `ticketflightassociatedwith_ibfk_4` FOREIGN KEY (`toAirport`) REFERENCES `airport` (`ThreeLetterID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ticketflightassociatedwith`
+--
+
+LOCK TABLES `ticketflightassociatedwith` WRITE;
+/*!40000 ALTER TABLE `ticketflightassociatedwith` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ticketflightassociatedwith` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -196,4 +528,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-12-01 21:55:54
+-- Dump completed on 2023-12-04 14:59:43
